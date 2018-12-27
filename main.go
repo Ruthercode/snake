f9ae68cb5cc667a4ec907ce3dfdf542dab220b8e
Ruthercode <ruthercode@gmail.com>
2018-12-27 20:55:40 +0300

    create map

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+var ycor int
+var xcor int
 
-// test commit
+func draw() {
+	for i := 0; i < 29; i++ {
+		fmt.Print("#")
+	}
+	fmt.Println("#")
+	for i := 0; i < 28; i++ {
+		fmt.Print("#")
+		for j := 0; j < 28; j++ {
+			if j == xcor && i == ycor {
+				fmt.Print("O")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println("#")
+	}
+
+	for i := 0; i < 30; i++ {
+		fmt.Print("#")
+	}
+}
 func main() {
-	fmt.Println("Hello world")
+	ycor = 15
+	xcor = 15
+	for true {
+		draw()
+		time.Sleep(time.Second * 2)
+	}
 }
