2f24ec2267a9bede8392cd93f1e50a0930df79a4
Ruthercode <ruthercode@gmail.com>
2019-02-01 20:34:44 +0300

    almost complete

--- main.go
+++ main.go
@@ -17,7 +17,23 @@ var (
 	foodX    int
 	foodY    int
 	score    int = 0
+	x_tail       = make([]int, 1)
+	y_tail       = make([]int, 1)
+	gps_x        = make([]int, 1)
+	gps_y        = make([]int, 1)
 )
+
+func tail_move() {
+	j := 0
+	if score != 0 {
+		for i := score - 1; i >= 0; i-- {
+			index := len(gps_x) - 1 - j
+			x_tail[i] = gps_x[index]
+			y_tail[i] = gps_y[index]
+			j++
+		}
+	}
+}
 
 func draw() {
 	for i := 0; i < 30; i++ {
@@ -33,7 +49,16 @@ func draw() {
 			} else if i == foodY && j == foodX {
 				fmt.Print("F")
 			} else {
-				fmt.Print(" ")
+				solve := 0
+				for k := 0; k < score; k++ {
+					if i == y_tail[k] && j == x_tail[k] {
+						fmt.Print("s")
+						solve++
+					}
+				}
+				if solve == 0 {
+					fmt.Print(" ")
+				}
 			}
 		}
 		fmt.Println("#")
@@ -55,6 +80,7 @@ func move() {
 		panic(err)
 	}
 	dir = string(c)
+
 	switch dir {
 	case "w":
 		ycor -= 1
@@ -66,8 +92,36 @@ func move() {
 		xcor += 1
 	}
 
+	gps_x = append(gps_x, xcor)
+	gps_y = append(gps_y, ycor)
+
 	if xcor == foodX && ycor == foodY {
 		foodAvbl = 0
+		if x_tail[0] == 0 {
+			x_tail[0] = xcor
+			y_tail[0] = ycor
+			if dir == "w" {
+				y_tail[0]++
+			} else if dir == "s" {
+				y_tail[0]--
+			} else if dir == "a" {
+				x_tail[0]++
+			} else if dir == "d" {
+				x_tail[0]--
+			}
+		} else {
+			x_tail = append(x_tail, xcor)
+			y_tail = append(y_tail, ycor)
+			if dir == "w" {
+				y_tail[score]++
+			} else if dir == "s" {
+				y_tail[score]--
+			} else if dir == "a" {
+				x_tail[score]++
+			} else if dir == "d" {
+				x_tail[score]--
+			}
+		}
 		score++
 	}
 
@@ -96,6 +150,7 @@ func main() {
 	xcor = 15
 	for true {
 		food()
+		tail_move()
 		move()
 		tools.Clear()
 		draw()
